e787c6817617114b7e26d39519079ed65d51967f
me-here <mihirthanekar@gmail.com>
2020-07-06 15:24:35 -0700

    Left + right buttons update calendar month

--- a.js
+++ a.js
@@ -1,3 +1,5 @@
+let currentDate = new Date();
+
 function getData(symbol, start, end, timeframe="D") {
     var req = new XMLHttpRequest();
     req.onreadystatechange = ()=>{
@@ -15,13 +17,13 @@ function getData(symbol, start, end, timeframe="D") {
 function processData(data) {
     processedClose = data.c
     let dates = document.querySelectorAll("#variable-start-space ~ div")
-    const offset = document.getElementById("variable-start-space").style.gridColumnEnd
+    offset = parseInt(document.getElementById("variable-start-space").style.gridColumnEnd)
     for(let i = 0; i < dates.length; i++) {
         // TODO: if not holiday
-        
+
 
         // Not a weekday
-        dayOfWeek = (parseInt(dates[i].innerHTML) + parseInt(offset) - 1) % 7;
+        dayOfWeek = (parseInt(dates[i].innerHTML) + offset - 1) % 7;
         if(dayOfWeek == 1 | dayOfWeek == 7) continue;
 
         // we didn't finish the month
@@ -29,15 +31,35 @@ function processData(data) {
 
         let color = "red";
         if(processedClose[i+1] - processedClose[i] >= 0) {   // if it closed higher than the previous day
+            let bubble = document.createElement("div"); 
             color = "green";
         }
         dates[i].style.backgroundColor = color;
+
+
     }
+
+    // Go through each of the dates
+    startColumn = offset;
+    for(let i = 0; i < dates.length; i++) {
+        if(i > processedClose.length - 2) break;
+        dayOfWeek = (parseInt(dates[i].innerHTML) + parseInt(offset) - 1) % 7;
+        if(dayOfWeek == 1 | dayOfWeek == 7) continue;
+
+        
+        /*
+        if(dates[i].style.backgroundColor != dates[i+1].style.backgroundColor) {
+            endColumn = i + 1 + offset;
+            console.log(startColumn, endColumn)
+            startColumn = i + 2 + offset;
+        }*/
+    }
+
+
 
 }
 
-function calculateCalendarRange() {
-    var date = new Date();
+function calculateCalendarRange(date=new Date()) {
     const year = date.getFullYear();
     const month = date.getMonth();
     
@@ -54,20 +76,28 @@ function calculateCalendarRange() {
     }
 }
 
-function offsetCalendar() {
-    const range = calculateCalendarRange();
+function offsetCalendar(date=new Date()) {
+    // Display current month and year in calendar
+    document.getElementById("month").innerHTML = new Intl.DateTimeFormat('en-US', {month: 'long', year: 'numeric'}).format(date);
+    
+    const range = calculateCalendarRange(date);
     if(range == null) return
 
     // Empty space at beginning of calendar
-    if(range.firstDayOfWeek != 0) { // if start date is not Sunday
-        document.getElementById("variable-start-space").style.display = "block";
-        document.getElementById("variable-start-space").style.gridColumnEnd = range.firstDayOfWeek;
-    } 
+    console.log(range.firstDayOfWeek)
+    
+    if(range.firstDayOfWeek == 1) {
+        document.getElementById("variable-start-space").style.display = "none";
+    }
+    document.getElementById("variable-start-space").style.gridColumnEnd = range.firstDayOfWeek;
+    console.log("fdsfasfsa ", document.getElementById("variable-start-space").style.gridColumnEnd)
 
     // Correct number of days in month
     for (let day of document.getElementsByClassName("last-days")) {
         if(parseInt(day.innerHTML) > parseInt(range.lastDayOfMonth)) {
             day.style.display = "none";
+        } else {
+            day.style.display = "block";
         }
     }
 }
@@ -82,9 +112,41 @@ function fetch() {
     getData(tickerSymbol, unixFromDate(range.lastDayLastMonth), unixFromDate(range.lastDate));
 }
 
+function configureGrid() {
+    let grid = document.querySelectorAll("#variable-start-space ~ div")
+    let offset = parseInt(document.getElementById("variable-start-space").style.gridColumnEnd) - 2
+    grid.forEach(element => {
+        let itemNum = parseInt(element.innerHTML) + offset // Day number + offset
+        let row = Math.floor(itemNum / 7) + 1   // default value 1
+        let column = itemNum % 7 + 1;
+        
+        element.style.gridColumnStart = column
+        element.style.gridRowStart = row
+    });
+}
+
+function changeMonth(newMonth) {
+    console.log(newMonth)
+    offsetCalendar(newMonth);
+    configureGrid();
+    return
+}
+
+
 window.onload = function() {
     offsetCalendar();
+    configureGrid();
     document.getElementById('search').addEventListener('click', ()=>{
         fetch();
+    });
+
+    document.getElementById('previousMonth').addEventListener('click', ()=>{
+        currentDate.setMonth(currentDate.getMonth() - 1);
+        changeMonth(currentDate);
+    });
+
+    document.getElementById('nextMonth').addEventListener('click', ()=>{
+        currentDate.setMonth(currentDate.getMonth() + 1);
+        changeMonth(currentDate);
     });
 }
